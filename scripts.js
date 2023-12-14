6f5da33517d5fa6bbc9b4e59ea6ae0343f72f80e
Montserrat <151891002+Montserrat115@users.noreply.github.com>
2023-12-13 19:07:28 -0600

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 function validarFormulario() {
-    //Verifica el correcto formato de los datos ingresados
     const nombre = document.getElementById("nombre").value;
     const apellidoPaterno = document.getElementById("a_parterno").value;
     const apellidoMaterno = document.getElementById("a_materno").value;
@@ -9,42 +8,44 @@ function validarFormulario() {
     const tel = document.getElementById("telefono").value;
     const fechaNacimiento = new Date(document.getElementById("nacimiento").value);
     const fechaActual = new Date();
+    var respuesta = window.confirm("¿Quieres modificar?");
+    const Formulario = document.querySelectorAll("formulario");
 
-    const exp = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/;
-    const exp1 = /^(PE|PP)\d{8}$|\d{10}$/;
-    const exp2 = /^[A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2}$/;
+    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/;
+    const regex1 = /^(PE|PP)\d{8}$|\d{10}$/;
+    const regex2 = /^[A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2}$/;
   
-    if (!exp1.test(boleta)) {
+    if (!regex1.test(boleta)) {
       alert("El campo de boleta no es válido.");
       return false;
     }
 
-    if (!exp.test(nombre)) {
+    if (!regex.test(nombre)) {
       alert("El campo de nombre solo debe contener letras.");
       return false;
     }
   
-    if (!exp.test(apellidoPaterno)) {
+    if (!regex.test(apellidoPaterno)) {
       alert("El campo de apellido paterno solo debe contener letras.");
       return false;
     }
   
-    if (!exp.test(apellidoMaterno)) {
+    if (!regex.test(apellidoMaterno)) {
       alert("El campo de apellido materno solo debe contener letras.");
       return false;
     }
 
-    if (!exp2.test(curp)) {
+    if (!regex2.test(curp)) {
         alert("El campo de CURP no es válido.");
         return false;
       }
 
-    if (exp.test(cp)) {
+    if (regex.test(cp)) {
         alert("El campo de código postal solo debe contener números.");
         return false;
     }
 
-    if (exp.test(tel)) {
+    if (regex.test(tel)) {
         alert("El campo de teléfono solo debe contener números.");
         return false;
     }
@@ -53,18 +54,21 @@ function validarFormulario() {
       alert("Asegurate que la fecha de nacimiento sea correcta.");
       return false;
     }
-   else{
-//Al dar clic en Registrar mostrar los datos ingresados (falta agregar datos y en dicha pantalla se le proporcionará al usuario dos opciones: Aceptar y
-Modificar.)
-    alert("Tu nombre es:" + " " + $('#nombre').val() + "\n");
-    return true;
-   }
 
+   else {
+    alert("Tu nombre es: " + $('#nombre').val() + "\n");
+    if (respuesta) {
+      alert("Modifica tus datos, no olvides presionar registrar al terminar.");
+      return false;
+    } else {
+      return true;
+    }
   }
-
-// este es para mostrar/ocultar el campo de "Otro" según lo que ponga usuario en nombre de escuela
-var otro = document.getElementById('otro');
-$("#otro").hide();
+}
+   
+  
+  var otro = document.getElementById('otro');
+  $("#otro").hide();
 
 function ocultarOtro() {
     var select = document.getElementById('escuela');
@@ -79,21 +83,10 @@ function ocultarOtro() {
   
  document.getElementById('escuela').addEventListener('change', ocultarOtro);
 
-//Borra todos los campos del formulario
 function limpiarFormulario() {
     const Formulario = document.querySelectorAll("formulario");
     Formulario.forEach((element) => {
       element.value = null;
-    });
-}
-
-//Mantiene todos los datos en el formulario luego del alert
-function modificarFormulario() {
-    // aqui para cargar datos supongo, la verdad no se que quieran póner aca jaja
-    alert("Modifica tus datos, no olvides presionar registrar al terminar.");
-    const Formulario = document.querySelectorAll("formulario");
-    Formulario.forEach((element) => {
-      element.value = element.value;
     });
 }
 
