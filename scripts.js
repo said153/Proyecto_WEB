d39b6ac2106e410e178953ad70b9eab50ae285cc
Montserrat <151891002+Montserrat115@users.noreply.github.com>
2023-12-14 22:45:02 -0600

    scripts.js

--- scripts.js
+++ scripts.js
@@ -13,8 +13,8 @@ function validarFormulario() {
 
     const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/;
     const regex1 = /^(PE|PP)\d{8}$|\d{10}$/;
-    const regex2 = /^[A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2}$/;
-  
+   // const regex2 = /^[A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z0-9]{2}$/;
+  const regex2 =  /^([A-Z&]|[a-z&]{1})([AEIOU]|[aeiou]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([HM]|[hm]{1})([AS|as|BC|bc|BS|bs|CC|cc|CS|cs|CH|ch|CL|cl|CM|cm|DF|df|DG|dg|GT|gt|GR|gr|HG|hg|JC|jc|MC|mc|MN|mn|MS|ms|NT|nt|NL|nl|OC|oc|PL|pl|QT|qt|QR|qr|SP|sp|SL|sl|SR|sr|TC|tc|TS|ts|TL|tl|VZ|vz|YN|yn|ZS|zs|NE|ne]{2})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([0-9]{2}|[A-Z0-9]{2}|[A-ZA-Z]{2}|[a-z0-9]{2}|[a-za-z]{2})$/;
     if (!regex1.test(boleta)) {
       alert("El campo de boleta no es válido.");
       return false;
