4b7d7be054bea6f68385f82fc4ecafa71dcbab52
martin cortes <cortesbuendiamartin@gmail.com>
2024-01-05 20:15:46 -0600

    Subo Avance de Pantalla de usuario despues del login, falta hacerlo responsive,  agregar texto y checar detalles

--- Proyecto_WEB_Modificado/js/scripts.js
+++ Proyecto_WEB_Modificado/js/scripts.js
@@ -173,3 +173,29 @@ document.getElementById("discapacidadMotriz").addEventListener("change", functio
   var opcionesMotriz = document.getElementById("opcionesMotriz");
   opcionesMotriz.style.display = this.checked ? "block" : "none";
 });
+
+
+
+//sombreado de imagenes en inicio de la sesion 
+document.addEventListener('DOMContentLoaded', function () {
+  const clickableElements = document.querySelectorAll('.clickable');
+
+  clickableElements.forEach(function (clickableElement) {
+    const h1Element = clickableElement.querySelector('h1');
+    const overlayText = clickableElement.querySelector('.overlay-text');
+
+    h1Element.addEventListener('click', function () {
+      console.log('Clic en el texto');
+      if (overlayText) {
+        overlayText.style.display = 'block';
+      }
+    });
+
+    clickableElement.addEventListener('click', function (event) {
+      const linkElement = clickableElement.querySelector('a');
+      if (linkElement) {
+        linkElement.click(); // Simular clic en el enlace si existe
+      }
+    });
+  });
+});
